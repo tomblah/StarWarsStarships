30d4208849da7e238073de4a4a80a30c061bcf17
Thomas Argue <tomargue@gmail.com>
2021-12-01 21:01:03 +1100

    wip - starship

--- StarWarsStarships/Model/Starship.swift
+++ StarWarsStarships/Model/Starship.swift
@@ -7,7 +7,11 @@
 
 import Foundation
 
-public struct Starship {
+public struct Starship: Codable {
+    
+    enum StarshipCodingKey: CodingKey {
+        case name, model, manufacturer, costInCredits, length, maxAtmospheringSpeed, crew, passengers, cargoCapacity, consumables, hyperdriveRating, mglt, starshipClass, pilots, films, created, edited, urlString
+    }
     
     // NB: the fields that we suspect we'll do math on, we'll have them as numbers, the rest as strings
 
@@ -30,6 +34,53 @@ public struct Starship {
     let created: Date
     let edited: Date
     let urlString: String
+    
+    // MARK: - Codable
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: StarshipCodingKey.self)
+        name = try container.decode(String.self, forKey: .name)
+        model = try container.decode(String.self, forKey: .model)
+        manufacturer = try container.decode(String.self, forKey: .manufacturer)
+        costInCredits = try container.decode(Double.self, forKey: .costInCredits)
+        length = try container.decode(String.self, forKey: .length)
+        maxAtmospheringSpeed = try container.decode(String.self, forKey: .maxAtmospheringSpeed)
+        crew = try container.decode(String.self, forKey: .crew)
+        passengers = try container.decode(Int.self, forKey: .passengers)
+        cargoCapacity = try container.decode(String.self, forKey: .cargoCapacity)
+        consumables = try container.decode(String.self, forKey: .consumables)
+        hyperdriveRating = try container.decode(String.self, forKey: .hyperdriveRating)
+        mglt = try container.decode(String.self, forKey: .mglt)
+        starshipClass = try container.decode(String.self, forKey: .starshipClass)
+        pilots = try container.decode([String].self, forKey: .pilots)
+        films = try container.decode([String].self, forKey: .films)
+        created = try Starship.dateFormatter.date(from: container.decode(Date.self, forKey: .created))! // FIXME: make less brittle
+        edited = try Starship.dateFormatter.date(from: container.decode(String.self, forKey: .edited))! // FIXME: make less brittle
+        urlString = try container.decode(String.self, forKey: .urlString)
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: StarshipCodingKey.self)
+        try container.encode(name, forKey: .name)
+        try container.encode(model, forKey: .model)
+        try container.encode(manufacturer, forKey: .manufacturer)
+        try container.encode(costInCredits, forKey: .costInCredits)
+        try container.encode(length, forKey: .length)
+        try container.encode(maxAtmospheringSpeed, forKey: .maxAtmospheringSpeed)
+        try container.encode(crew, forKey: .crew)
+        try container.encode(passengers, forKey: .passengers)
+        try container.encode(cargoCapacity, forKey: .cargoCapacity)
+        try container.encode(consumables, forKey: .consumables)
+        try container.encode(hyperdriveRating, forKey: .hyperdriveRating)
+        try container.encode(starshipClass, forKey: .starshipClass)
+        try container.encode(mglt, forKey: .mglt)
+        try container.encode(starshipClass, forKey: .starshipClass)
+        try container.encode(pilots, forKey: .pilots)
+        try container.encode(films, forKey: .films)
+        try container.encode(created, forKey: .created)
+        try container.encode(edited, forKey: .edited)
+        try container.encode(urlString, forKey: .urlString)
+    }
     
 }
 
